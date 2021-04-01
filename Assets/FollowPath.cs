d18c2cd1770c8e35ec0a868413049bea1267c1fb
imperadordf <49728767+imperadordf@users.noreply.github.com>
2021-04-01 17:03:26 -0300

    Added Project

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+ 
+     //Inicia o Script com os parametros que ele recebe, waypoints e Graph
+    public void Inicialize(GameObject[] waypoints,Graph graph) 
+    {
+        wps = waypoints;
+        g = graph;
+        currentNode = wps[0];
+    }
+    
+    //Ele inicia um metodo que A* para achar o melhor caminho para  o Heli
+    public void GoToHeli()
+    {
+        //Meotod A*, que recebe o pronto que esta no momento e qual é o destino.
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+
+    //Ele inicia um metodo que A* para achar o melhor caminho para  a Ruina
+    public void GoToRuin()
+    {
+        //Meotod A*, que recebe o pronto que esta no momento e qual é o destino.
+        g.AStar(currentNode, wps[6]);
+        //Reseta a current que ele esta
+        currentWP = 0;
+    }
+
+    private void LateUpdate()
+    {
+      
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        currentNode = g.getPathPoint(currentWP);
+        //Quando estiver chegando perto ele muda para proximo ponto
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+        //Continue enquanto a currente for menor que a lista de caminho
+        if (currentWP < g.getPathLength())
+        {
+            //Ação que faz o tank seguir a direção para o ponto
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        }
+        // o que faz o tank andar no eixo Z 
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    //Variavel de transform que pega a proxima direção
+    Transform goal;
+    //velocidade de andar e rotação, mais a distancia que ele chega
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    //A lista de pontos
+    GameObject[] wps;
+    //o objeto que ele esta, o waypoint que esta, o Node
+    GameObject currentNode;
+    //Contador
+    int currentWP = 0;
+    //A classe Graph que faz toda a logica para andar e buscar o proximo ponto e A*
+    Graph g;
+
+}
---
+++ Assets/WPManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] 
+//A classe que faz as direção
+public struct Link{
+    //a direção ser BI OU UNI, mais a direção que seria os dois gameobjects
+    public enum direction { UNI, BI }
+    public GameObject node1; 
+    public GameObject node2; 
+    public direction dir;}
+
+public class WPManager : MonoBehaviour
+{
+    //array de Waypoints
+    public GameObject[] waypoints; 
+    //A direção que ele pode fazer, com o Link, direção Bi e Uni
+    public Link[] links; 
+    //A classe G, para gerenciar os wapoints e os links
+    public Graph graph = new Graph();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gerenciador e colocando os waypoints no Graph e os links, causa a lista de waypoints for maior que 0
+        if (waypoints.Length > 0)
+        {
+            foreach(GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+            foreach (Link l in links)
+            {
+                //Adiciona uma direção, caso for BI, ele adiciona mais uma que seria a ida e a volta
+                graph.AddEdge(l.node1, l.node2);
+                if (l.dir == Link.direction.BI)
+                    graph.AddEdge(l.node2, l.node1);
+            }
+            //Inicia o script de Follow, dando os parametros que ele recebe (eu que fiz KKKK, treinando essa norma)
+            follow.Inicialize(graph:graph,waypoints:waypoints);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Desenhar
+        graph.debugDraw();
+    }
+
+    [SerializeField] private FollowPath follow;
+}
